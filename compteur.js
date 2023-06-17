f8906369184e066a1e985e62198b5d714ccb2f9f
Nathanaël <nathanael.leang@gmail.com>
2023-06-18 01:16:53 +0200

    clicker

--- compteur.js
+++ compteur.js
@@ -1,6 +1,8 @@
 console.log('Bonjour javascript');
 
-var nbclick = 0
+var nbclick = 0;
+
+var title = "";
 
 function compteur() {
     nbclick++;
@@ -8,3 +10,37 @@ function compteur() {
 }
 
 document.getElementById("bouton").addEventListener("click", compteur);
+
+function change() {
+    if (nbclick > 9){
+        var title = "Trainee"
+    document.getElementById("name").textContent = title;
+    }
+
+    if (nbclick > 50){
+        var title = "Advanced"
+    document.getElementById("name").textContent = title;
+    }
+    if (nbclick > 150){
+        var title = "Certified"
+    document.getElementById("name").textContent = title;
+    }
+    if (nbclick > 300){
+        var title = "Pro"
+    document.getElementById("name").textContent = title;
+    }
+    if (nbclick > 500){
+        var title = "Master"
+    document.getElementById("name").textContent = title;
+    }
+    if (nbclick > 1000){
+        var title = "GOD"
+    document.getElementById("name").textContent = title;
+    }
+    if (nbclick > 2000){
+        var title = "WTF ! WHO YOU ARE ?"
+    document.getElementById("name").textContent = title;
+    }
+}
+
+document.getElementById("bouton").addEventListener("click", change);
